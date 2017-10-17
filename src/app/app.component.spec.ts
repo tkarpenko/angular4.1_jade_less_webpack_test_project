27ac02a8857d152d26eaa2e8c706427189c79d39
Tania Karpenko <tania-karpenko@ukr.net>
2017-10-17 10:35:57 +0300

    fixes

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,5 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { AppComponent } from './app.component';
